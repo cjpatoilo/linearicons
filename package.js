77853c9eb9b86ae1d4d4be1163ec3b0bf90b05e5
CJ Patoilo <cjpatoilo@gmail.com>
2016-10-06 23:55:23 -0300

    Added support to composer and meteor package manager

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+	name: "cjpatoilo:linearicons",
+	version: "1.0.0",
+	summary: "Linearicons is the highest quality set of line icons, matching with minimalist UI designs in iOS.",
+	git: "https://github.com/cjpatoilo/linearicons.git",
+	documentation: "README.md"
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom("METEOR@1.0");
+	api.addFiles([
+		"dist/web-font/style.css"
+	], "client");
+});
