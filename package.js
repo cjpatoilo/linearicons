57bd5307792a876293342a0cca3800abe9c365ec
CJ Patoilo <cjpatoilo@gmail.com>
2017-11-08 00:25:07 -0200

    Build v1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: "cjpatoilo:linearicons",
-	version: "1.0.0",
+	version: "1.0.1",
 	summary: "Linearicons is the highest quality set of line icons, matching with minimalist UI designs in iOS.",
 	git: "https://github.com/cjpatoilo/linearicons.git",
 	documentation: "README.md"
